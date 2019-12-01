a9b9b43c2e5dbdb8c0a3e7cedf98790f93283018
warlord <warlord@warlord.com>
2019-12-01 14:12:27 +0530

    header and status

--- go_http_mux.go
+++ go_http_mux.go
@@ -4,11 +4,16 @@ import (
 	"net/http"
 )
 
-const message = "some sample response"
+const (
+	message         = "some sample response"
+	contenTypeValue = "text/html; charset=utf-91"
+)
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(writer http.ResponseWriter, req *http.Request) {
+		writer.Header().Set("Content-Type", contenTypeValue) // Tip : this should be written before calling writer.WriteHeader
+		writer.WriteHeader(http.StatusUnauthorized)
 		writer.Write([]byte(message))
 	})
 	http.ListenAndServe(":8080", mux)
